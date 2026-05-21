3fffb0301603387db195cc44503abf04da15d7ed
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2026-05-21 16:59:40 +0530

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects";
+import cors from 'cors'
 
 const app = express();
 const PORT = 8000;
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 app.use(express.json());
-app.get('/',(req,res)=>{
+
+app.use('/api/subjects', subjectsRouter)
+app.get('/', (req, res) => {
   res.send('hello,Welcome to Classroom API!');
 })
 
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -67,3 +67,6 @@ router.get('/', async (req, res) => {
         res.status(500).json({error: 'Failed to get subjects'});
     }
 })
+
+
+export default router
